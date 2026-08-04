2f29cb409f210fe4b97ce7dd13af06f521a5d944
Eero040 <142992981+Eero040@users.noreply.github.com>
2026-08-05 01:43:57 +0300

    da

--- Kalenteri/scripts/weekcalendar.js
+++ Kalenteri/scripts/weekcalendar.js
@@ -1,13 +1,19 @@
-import { generateWeekDays } from "./date.js";
+import { generateWeekDays, isTheSameDay, today } from "./date.js";
+import { isEventAllDay } from "./event.js"
+import { initEventList } from "./eventlist.js"
 
 const calendarTemplateElement = document.querySelector(
   "[data-template='week-calendar']");
 const calendarDayOfWeekTemplateElement = document.querySelector(
-  "[data-template='week-calendar-day-of-week']");
+  "[data-template='week-calendar-day-of-week-day']");
 const calendarAllDayListTemplateElement = document.querySelector(
   "[data-template='week-calendar-all-day-list-item']");
 const calendarColumnTemplateElement = document.querySelector(
   "[data-template='week-calendar-column']");
+
+const dateFormatter = new Intl.DateTimeFormat("fi-FI", {
+  weekDay: 'short'
+});
 
 export function initWeekCalendar(parent, selectedDate, eventStore) {
   const calendarContent = calendarTemplateElement.content.cloneNode(true);
@@ -20,5 +26,37 @@ export function initWeekCalendar(parent, selectedDate, eventStore) {
     "[data-week-calendar-columns]");
 
   const weekDays = generateWeekDays(selectedDate);
-  console.log(weekDays);
+  for (const weekDay of weekDays) {
+    const events = eventStore.getEventsByDate(weekDay);
+    const allDayEvents = events.filter((event) => isEventAllDay
+      (event));
+    initDayOfWeek(calendarDayOfWeekListElement, selectedDate, weekDay);
+    initAllDayListItem(calendarAllDayListElement, allDayEvents);
+  }
+
+  parent.appendChild(calendarElement);
+}
+
+function initDayOfWeek(parent, selectedDate, weekDay) {
+  const calendarDayOfWeekContent = calendarDayOfWeekTemplateElement.content.cloneNode(true);
+  const calendarDayOfWeekElement = calendarDayOfWeekContent.querySelector("[data-week-calendar-day-of-week]");
+  const calendarDayOfWeekButtonElement = calendarDayOfWeekElement.querySelector("[data-week-calendar-day-of-week-button]");
+  const calendarDayOfWeekDayElement = calendarDayOfWeekElement.querySelector("[data-week-calendar-day-of-week-day]");
+  const calendarDayOfWeekNumberElement = calendarDayOfWeekElement.querySelector("[data-week-calendar-day-of-week-number]");
+
+  calendarDayOfWeekNumberElement.textContent = weekDay.getDate();
+  calendarDayOfWeekDayElement.textContent = dateFormatter.format(weekDay);
+
+  if (isTheSameDay(weekDay, today())) {
+    calendarDayOfWeekButtonElement.classList.add("week-calendar_day-of-week-button-highlight");
+  }
+
+  parent.appendChild(calendarDayOfWeekElement);
+}
+
+function initAllDayListItem(parent, events) {
+  const calendarAllDayListItemContent = calendarAllDayListTemplateElement.content.cloneNode(true);
+  const calendarAllDayListItemElement = calendarAllDayListItemContent.querySelector("[data-week-calendar-all-day-list-item]");
+  initEventList(calendarAllDayListItemElement, events);
+  parent.appendChild(calendarAllDayListItemElement);
 }
